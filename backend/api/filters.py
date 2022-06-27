33b05a84a2b41220beeba44154cf35e27f35a85b
Natalya <ngkolyadina@1367.ru>
2022-06-28 00:09:06 +0300

    first commit

---
+++ backend/api/filters.py
@@ -0,0 +1,49 @@
+import django_filters as filters
+from django.core.exceptions import ValidationError
+from recipes.models import Ingredient, Recipe
+from users.models import User
+
+
+class MultipleChoiceFieldForTags(
+        filters.fields.MultipleChoiceField):
+    def validate(self, value):
+        if self.required and not value:
+            raise ValidationError(
+                self.error_messages['required'],
+                code='required')
+        for v in value:
+            if v in self.choices and not self.valid_value(v):
+                raise ValidationError(
+                    self.error_messages['invalid_choice'],
+                    code='invalid_choice',
+                    params={'value': v},)
+
+
+class TagsFilter(filters.AllValuesMultipleFilter):
+    field_class = MultipleChoiceFieldForTags
+
+
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all())
+    is_in_shopping_cart = filters.BooleanFilter(
+        widget=filters.widgets.BooleanWidget(),
+        label='Находится в корзине')
+    is_favorited = filters.BooleanFilter(
+        widget=filters.widgets.BooleanWidget(),
+        label='Находится в избранных.')
+    tags = filters.AllValuesMultipleFilter(
+        field_name='tags__slug',
+        label='Ссылка на рецепт')
+
+    class Meta:
+        model = Recipe
+        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
---
+++ backend/users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'username', 'email',
+        'first_name', 'last_name', 'date_joined',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('date_joined', 'first_name', 'email')
+    empty_value_display = '-пусто-'
