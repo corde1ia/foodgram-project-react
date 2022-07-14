91eb438fae3fc1cb76e078421d90c0cb7a40fe52
Natalya <ngkolyadina@1367.ru>
2022-07-14 14:20:22 +0300

    new

--- backend/api/views.py
+++ backend/api/views.py
@@ -142,9 +142,20 @@ class AddDeleteFavoriteRecipe(
 class AddDeleteShoppingCart(
         generics.RetrieveDestroyAPIView,
         generics.ListCreateAPIView):
-    """Добавление и удаление рецепта из корзины."""
+    """Добавление и удаление рецепта в/из избранных."""
 
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = SubscribeRecipeSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.favorite_recipe
+
+    def get_object(self):
+        recipe_id = self.kwargs['recipe_id']
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        self.check_object_permissions(self.request, recipe)
+        return recipe
 
     def create(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -154,6 +165,23 @@ class AddDeleteShoppingCart(
 
     def perform_destroy(self, instance):
         self.request.user.shopping_cart.recipe.remove(instance)
+
+
+# class AddDeleteShoppingCart(
+#         generics.RetrieveDestroyAPIView,
+#         generics.ListCreateAPIView):
+#     """Добавление и удаление рецепта из корзины."""
+
+#     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+#     def create(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         request.user.shopping_cart.recipe.add(instance)
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#     def perform_destroy(self, instance):
+#         self.request.user.shopping_cart.recipe.remove(instance)
 
 
 class AuthToken(ObtainAuthToken):
