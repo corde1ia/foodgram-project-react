8f6c55982f8347605e589087f09b11f34a209b8b
Natalya <ngkolyadina@1367.ru>
2022-07-14 21:35:33 +0300

    models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core import validators
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from foodgram import settings as s
 
 User = get_user_model()
