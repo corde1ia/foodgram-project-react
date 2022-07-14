05fd983255fbe52579b49b7d64c9eb86dac4a27c
Natalya <ngkolyadina@1367.ru>
2022-07-14 15:52:19 +0300

    24

--- backend/api/views.py
+++ backend/api/views.py
@@ -112,7 +112,7 @@ class AddDeleteFavoriteRecipe(
 
     def create(self, request, *args, **kwargs):
         instance = self.get_object()
-        request.user.favorite_recipe.recope.add(instance)
+        request.user.favorite_recipe.recipe.add(instance)
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
