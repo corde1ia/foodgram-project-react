98f6f6ae51d9cebd17885bc2559493ffafa827c8
Natalya <ngkolyadina@1367.ru>
2022-07-14 15:06:31 +0300

    recipe

--- backend/api/views.py
+++ backend/api/views.py
@@ -103,7 +103,7 @@ class AddDeleteFavoriteRecipe(
 
     def get_queryset(self):
         user = self.request.user
-        return user.favorite_recipe
+        return user.recipe.favorite_recipe
 
     def get_object(self):
         recipe_id = self.kwargs['recipe_id']
@@ -113,12 +113,12 @@ class AddDeleteFavoriteRecipe(
 
     def create(self, request, *args, **kwargs):
         instance = self.get_object()
-        request.user.favorite_recipe.recipe.add(instance)
+        request.user.recipe.favorite_recipe.add(instance)
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def perform_destroy(self, instance):
-        self.request.user.favorite_recipe.recipe.remove(instance)
+        self.request.user.recipe.favorite_recipe.remove(instance)
 
 
 # class AddDeleteFavoriteRecipe(
